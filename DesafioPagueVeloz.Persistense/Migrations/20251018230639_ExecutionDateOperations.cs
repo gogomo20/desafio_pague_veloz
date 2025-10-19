a1612dc7dcea4e293222259aab8a36744b4af4ac
gogomo20 <erick.allan1oliveira@gmail.com>
2025-10-18 20:23:42 -0400

    adicionado data da operação

---
+++ DesafioPagueVeloz.Persistense/Migrations/20251018230639_ExecutionDateOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioPagueVeloz.Persistense.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExecutionDateOperations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExecutionDate",
+                table: "Operation",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExecutionDate",
+                table: "Operation");
+        }
+    }
+}
